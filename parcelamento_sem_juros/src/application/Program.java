e3eabeec6c54b1d2e89134025cfa8581f173bf9f
EttoreDezan <dezan3@outlook.com>
2021-01-12 20:41:34 -0300

    Alteracoes finais(VALIDACOES).

--- parcelamento_sem_juros/src/application/Program.java
+++ parcelamento_sem_juros/src/application/Program.java
@@ -9,29 +9,49 @@ public class Program {
 		int qtdParcelas;
 		int resto;
 		int valorCompraFinal;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Informe o valor da compra: ");
-		valorDaCompra = sc.nextInt();
-		
-		System.out.println("Quantidade de parcelas: ");
-		qtdParcelas = sc.nextInt();
-		
+
+		do {
+			System.out.println("Informe o valor da compra: ");
+			valorDaCompra = sc.nextInt();
+
+			if (valorDaCompra < 10) {
+				System.out.println("Informe um valor maior!");
+			} else if (valorDaCompra > 1000) {
+				System.out.println("Informe um valor menor!");
+			}
+		} while (valorDaCompra < 10 || valorDaCompra > 1000);
+
+		do {
+			System.out.println("Quantidade de parcelas: ");
+			qtdParcelas = sc.nextInt();
+
+			if (qtdParcelas < 2) {
+				System.out.println("Informe maior!");
+			} else if (qtdParcelas > 18) {
+				System.out.println("Informe um valor menor!");
+			}
+		} while (qtdParcelas < 2 || qtdParcelas > 18);
+
 		System.out.println("\n----- Parcelas à pagar -----\n");
-		
-		valorCompraFinal =  valorDaCompra / qtdParcelas;
-		
-		if (valorDaCompra % qtdParcelas != 0) {
+
+		valorCompraFinal = valorDaCompra / qtdParcelas;
+
+		if (valorDaCompra % qtdParcelas != 0) { // Se houver sobra na divisao, a mesma sera distribuida em 1 para X  parcelas iniciais(qtdParcelas -= resto).
+												
 			resto = valorDaCompra % qtdParcelas;
 			qtdParcelas -= resto;
-			
-			System.out.printf("%dx de R$%d,00\n", resto, valorCompraFinal + 1);
+
+			for (int i = 0; i < resto; i++) {
+				System.out.println(valorCompraFinal + 1);
+			}
 		}
-		
-		System.out.printf("%dx de R$%d,00\n", qtdParcelas, valorCompraFinal);
-	
-		 
-		sc.close();	
-	 }	
+
+		for (int i = 0; i < qtdParcelas; i++) {
+			System.out.println(valorCompraFinal);
+		}
+
+		sc.close();
+	}
 }
